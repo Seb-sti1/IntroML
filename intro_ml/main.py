e5693d4cd32dfec3c9c059a1c6b9cf7dbf8d3762
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-09-17 18:34:50 +0200

    add plots, start svg

--- intro_ml/main.py
+++ intro_ml/main.py
@@ -1,8 +1,42 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.linalg import svd
+
 from intro_ml.load_data import get_data
-from intro_ml.plot import scatter_by_class
+from intro_ml.plot import boxplot, histplot, corr_matrix
+
+
+def pca():
+    wine_data_no_class = wine_data.drop("class", axis=1)
+    wine_data_np = wine_data_no_class.to_numpy()
+    Y = wine_data_np - np.ones((wine_data_np.shape[0], 1)) * wine_data_np.mean(axis=0)
+
+    # PCA by computing SVD of Y
+    U, S, V = svd(Y, full_matrices=False)
+
+    # Compute variance explained by principal components
+    rho = (S * S) / (S * S).sum()
+
+    threshold = 0.9
+
+    # Plot variance explained
+    plt.figure()
+    plt.plot(range(1, len(rho) + 1), rho, "x-")
+    plt.plot(range(1, len(rho) + 1), np.cumsum(rho), "o-")
+    plt.plot([1, len(rho)], [threshold, threshold], "k--")
+    plt.title("Variance explained by principal components")
+    plt.xlabel("Principal component")
+    plt.ylabel("Variance explained")
+    plt.legend(["Individual", "Cumulative", "Threshold"])
+    plt.grid()
+    plt.show()
+
 
 if __name__ == '__main__':
     wine_data = get_data()
-    print(wine_data)
 
-    scatter_by_class(wine_data, "Alcohol", "Hue")
+    # scatter_by_class(wine_data, "Alcohol", "Hue")
+    corr_matrix(wine_data)
+    boxplot(wine_data)
+    histplot(wine_data)
+    pca()
--- intro_ml/plot.py
+++ intro_ml/plot.py
@@ -1,3 +1,4 @@
+import numpy as np
 from matplotlib import pyplot as plt
 
 
@@ -16,3 +17,38 @@ def scatter_by_class(wine_data, x_axis_column, y_axis_column, group_column="clas
 
     plt.show()
 
+
+def corr_matrix(wine_data):
+    # TODO add number on the plot
+    corr = wine_data.corr()
+    figure = plt.figure(figsize=(10, 8))
+    axes = figure.add_subplot(111)
+    caxes = axes.matshow(corr, )
+    figure.colorbar(caxes)
+    plt.xticks(rotation=90)
+    axes.set_xticks(np.arange(len(wine_data.columns)))
+    axes.set_yticks(np.arange(len(wine_data.columns)))
+    axes.set_xticklabels(list(wine_data.columns))
+    axes.set_yticklabels(list(wine_data.columns))
+    figure.tight_layout()
+    plt.show()
+
+
+def boxplot(wine_data):
+    normalized = wine_data / wine_data.max(axis=0)
+    normalized = normalized.drop("class", axis=1)
+
+    fig = plt.figure(figsize=(10, 8))
+    axes = fig.add_subplot(111)
+    normalized.plot.box(ax=axes)
+    plt.xticks(rotation=90)
+    fig.tight_layout()
+    plt.show()
+
+
+def histplot(wine_data):
+    # TODO add lines
+    wine_data_no_class = wine_data.drop("class", axis=1)
+    wine_data_no_class.hist(bins=30, figsize=(10, 10), layout=(5, 3))
+    plt.tight_layout()
+    plt.show()
