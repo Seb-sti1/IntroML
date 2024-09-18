778bf87880dc79c63825fa1724a306b773799b85
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-09-18 19:53:42 +0200

    feat: finish plots

--- intro_ml/main.py
+++ intro_ml/main.py
@@ -1,7 +1,11 @@
+import numpy as np
+import pandas as pd
+import seaborn as sb
+from matplotlib import pyplot as plt
 from scipy.linalg import svd
 
 from intro_ml.load_data import get_data
-from intro_ml.plot import histplot, corr_matrix, boxplot, plot_pca_variance
+from intro_ml.plot import scatter_by_class, plot_pca_variance
 
 if __name__ == '__main__':
     wine_data = get_data()
@@ -20,4 +24,23 @@ if __name__ == '__main__':
 
     plot_pca_variance(S)
 
+    fig = plt.figure(figsize=(10, 8))
+    sb.heatmap(Vh, yticklabels=wine_data_no_class.columns,
+               xticklabels=[f"PCA {i + 1}" for i in range(Vh.shape[0])],
+               cmap='Blues', annot=True, fmt='.2f')
+    fig.tight_layout()
+    plt.show()
 
+    Y_t = np.matmul(Y, Vh)
+    wine_data_t = pd.DataFrame(np.concatenate([wine_data["class"].to_numpy()[:, np.newaxis], Y_t], axis=1),
+                               columns=["class"] + [f"PCA {i + 1}" for i in range(Vh.shape[0])])
+    scatter_by_class(wine_data_t, "PCA 1", "PCA 2")
+    scatter_by_class(wine_data_t, "PCA 1", "PCA 3")
+    scatter_by_class(wine_data_t, "PCA 1", "PCA 4")
+    scatter_by_class(wine_data_t, "PCA 1", "PCA 5")
+    scatter_by_class(wine_data_t, "PCA 2", "PCA 3")
+    scatter_by_class(wine_data_t, "PCA 2", "PCA 4")
+    scatter_by_class(wine_data_t, "PCA 2", "PCA 5")
+    scatter_by_class(wine_data_t, "PCA 3", "PCA 4")
+    scatter_by_class(wine_data_t, "PCA 3", "PCA 5")
+    scatter_by_class(wine_data_t, "PCA 4", "PCA 5")
--- intro_ml/plot.py
+++ intro_ml/plot.py
@@ -6,11 +6,14 @@ from matplotlib import pyplot as plt
 def scatter_by_class(wine_data, x_axis_column, y_axis_column, group_column="class"):
     groups = wine_data.groupby(group_column)
 
+    colors = ['green', 'pink', 'blue']
+
     fig, ax = plt.subplots()
     ax.margins(0.05)
-    for name, group in groups:
+    for i, (name, group) in enumerate(groups):
         ax.plot(group[x_axis_column],
                 group[y_axis_column],
+                color=colors[i],
                 marker='o', linestyle='', ms=12, label=name)
     plt.xlabel(x_axis_column)
     plt.ylabel(y_axis_column)
