012b4026e8375e2e75386ffddc1fe72c24d5ec41
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-09-18 15:38:58 +0200

    fix: column names

--- intro_ml/load_data.py
+++ intro_ml/load_data.py
@@ -3,6 +3,13 @@ import os
 import pandas as pd
 from ucimlrepo import fetch_ucirepo
 
+column_correction = {"Alcalinity_of_ash": "Alkalinity of ash",
+                     "Total_phenols": "Total phenols",
+                     "Flavanoids": "Flavonoids",
+                     "Nonflavanoid_phenols": "Non flavonoid phenols",
+                     "Color_intensity": "Color intensity",
+                     "0D280_0D315_of_diluted_wines": "0D280 0D315 of diluted wines"}
+
 
 def fetch_data(print_info=False):
     """
@@ -23,12 +30,16 @@ def get_data():
     """
     :return: the data as a pandas dataframe
     """
+
     if os.path.exists("wine.csv"):
-        return pd.read_csv("wine.csv")
+        df = pd.read_csv("wine.csv")
+        df = df.rename(columns=column_correction)
+        df.to_csv("wine.csv", index=False)
+        return df
     else:
         X, y = fetch_data()
-
         data = pd.concat([y, X], axis=1)
+        data = data.rename(columns=column_correction)
         data.to_csv("wine.csv", index=False)
         return data
 
